899ecded3340aff5da24e697ee5dfab0b1257047
Karthik <kartisuri@gmail.com>
2018-08-15 17:43:15 +0800

    resolved conflict

--- src/test/java/Gauge/StepImplementation.java
+++ src/test/java/Gauge/StepImplementation.java
@@ -4,12 +4,18 @@ import com.thoughtworks.gauge.ContinueOnFailure;
 import com.thoughtworks.gauge.Gauge;
 import com.thoughtworks.gauge.Step;
 import driver.Driver;
+import org.apache.commons.compress.archivers.ar.ArArchiveEntry;
+import org.assertj.core.api.AbstractCharSequenceAssert;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import utils.WebElementDataStore;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -40,7 +46,7 @@ public class StepImplementation {
     public void ClickLoginButton() {
         WebElementDataStore.getWebElementByUniqueId("loginButton").click();
     }
-
+    @ContinueOnFailure
     @Step("Login as SuperUser")
     public void LoginSuperUser() {
         String userName = System.getenv("USER_NAME");
@@ -49,21 +55,25 @@ public class StepImplementation {
         EnterPassword(password);
         ClickLoginButton();
 
-        assertThat(WebElementDataStore.getWebElementByUniqueId("loginVerification")
-                .getText()
-                .trim()
-                .equalsIgnoreCase("Hello, SuperUser SuperUser"));
-    }
+        try {
+            TimeUnit.SECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
+        assertThat(Driver.webDriver.findElement(By.xpath("/html/body/div/div/div/div[2]/div/div[2]/div[2]")).getText().contains("Hello, SuperUser SuperUser"));
+
+    }
+    @ContinueOnFailure
     @Step("Add HR Director as Reviewer <FirstName> <LastName> <Email> <AdminLevel> <Hive> <Job Position>")
     public void AddReviewer(String firstName, String lastName, String email, String adminLevel, String hive, String jobPosition) {
 
-        // Click "Add Reviewer"
+        // Click Manage Reviewer --> "Add Reviewer"
 
-        Driver.webDriver.findElement(By.xpath("//*[@id=\"main-app-area\"]/div/div/div[2]/div/div[2]/div[1]/div[3]")).click();
+        Driver.webDriver.findElement(By.cssSelector("#main-app-area > div > div > div.body-admin > div > div.menu-staff > div.menu-staff-left > div:nth-child(3)")).click();
         assertThat(Driver.webDriver.findElement(By.xpath("//*[@id=\"add-a-new-reviewer\"]/div/div[1]")).getText().contains("Add a new reviewer"));
 
-        // Identity Reviewer Details Page & Enter details
+        // Assert Reviewer Details Page & Enter details
 
         Driver.webDriver.findElement(By.xpath("//*[@id=\"add-a-new-reviewer\"]/div/div[1]")).click();
         assertThat(Driver.webDriver.findElement(By.xpath("//*[@id=\"main-app-area\"]/div/div/div[1]/div/div[2]/div/div[1]/div[2]")).getText().contains("REVIEWER DETAILS"));
@@ -81,8 +91,38 @@ public class StepImplementation {
         Select JobPosition = new Select(Driver.webDriver.findElement(By.xpath("//*[@id=\"main-app-area\"]/div/div/div[1]/div/div[2]/div/div[2]/div[3]/div[3]/select")));
         JobPosition.selectByVisibleText(jobPosition);
 
+        String status = Driver.webDriver.findElement(By.className("new-reviewer-send-button")).getAttribute("disabled");
+        System.out.println(status);
+        assertThat(status).isNull();
+
+
+        Driver.webDriver.findElement(By.xpath("//*[@id=\"main-app-area\"]/div/div/div[1]/div/div[2]/div/div[2]/div[4]/div")).click();
+
+
+        try {
+            TimeUnit.SECONDS.sleep(15);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //Verify Reviewer in "Manage Reviewers"
+
+        String tableValue = Driver.webDriver.findElement(By.xpath("//*[@id=\"add-a-new-reviewer\"]/div/div[5]/div/div")).getText();
+
+        List<String> tableListValues = Arrays.asList(tableValue.split("\n"));
+        String checkPoint = firstName + " " + lastName;
+
+        int occurrences = Collections.frequency(tableListValues,checkPoint);
+
+        assertThat(occurrences).isEqualTo(1);
+
+        int i = tableListValues.indexOf(checkPoint);
+
+        assertThat(i).isGreaterThanOrEqualTo(0);
+        assertThat(checkPoint).isEqualToIgnoringCase(tableListValues.get(i).trim());
+        assertThat(jobPosition).isEqualToIgnoringCase(tableListValues.get(++i).trim());
+        assertThat(hive).isEqualToIgnoringCase(tableListValues.get(++i).trim());
+        assertThat(adminLevel).isEqualToIgnoringCase(tableListValues.get(++i).trim());
 
     }
-
-
 }
