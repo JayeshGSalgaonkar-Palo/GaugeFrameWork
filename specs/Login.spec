7d8cd11cc2a64159a627c99ff31e8394a5db325e
Jayesh Salgaonkar <jsalgaonkar@palo-it.com>
2018-08-15 17:17:35 +0800

    Complete Add Reviewer flow

--- specs/Login.spec
+++ specs/Login.spec
@@ -5,13 +5,16 @@ Created by Jayesh Salgaonkar on 14/8/2018
 This is an executable specification file which follows markdown syntax.
 Every heading in this file denotes a scenario. Every bulleted point denotes a step.
 
+table:InputTable.csv
+
 *Access NewBee Website
 
 ## User Creation
 Tags: UserCreation
 * Login as SuperUser
 
-* Add HR Director as Reviewer "FirstName" "LastName" "Email" "AdminLevel" "Hive" "Job Position"
+* Add HR Director as Reviewer <FirstName> <LastName> <Email> <AdminLevel> <Hive> <Job Position>
+
 
 //* Add Digital Technology Director as Reviewer
 
@@ -20,5 +23,5 @@ Tags: UserCreation
 ## Test AdminLevel, Hive, JobPosition Validations for all 3 Directors
 Tags: Validations
 
-* Login as SuperUser
-
+//* Login as SuperUser
+//* Duplicate Email address
--- src/test/java/Gauge/StepImplementation.java
+++ src/test/java/Gauge/StepImplementation.java
@@ -4,11 +4,17 @@ import com.thoughtworks.gauge.ContinueOnFailure;
 import com.thoughtworks.gauge.Gauge;
 import com.thoughtworks.gauge.Step;
 import driver.Driver;
+import org.apache.commons.compress.archivers.ar.ArArchiveEntry;
+import org.assertj.core.api.AbstractCharSequenceAssert;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -80,7 +86,7 @@ public class StepImplementation {
         Driver.webDriver.findElement(By.className("welcome-page-input-submit")).click();
 
     }
-
+    @ContinueOnFailure
     @Step("Login as SuperUser")
     public void LoginSuperUser() {
         String userName = System.getenv("USER_NAME");
@@ -89,19 +95,25 @@ public class StepImplementation {
         EnterPassword(password);
         ClickLoginButton();
 
-        assertThat(Driver.webDriver.findElement(By.className("menu-staff-right")).getText().contains("Hello, SuperUser SuperUser"));
+        try {
+            TimeUnit.SECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        assertThat(Driver.webDriver.findElement(By.xpath("/html/body/div/div/div/div[2]/div/div[2]/div[2]")).getText().contains("Hello, SuperUser SuperUser"));
 
     }
-
+    @ContinueOnFailure
     @Step("Add HR Director as Reviewer <FirstName> <LastName> <Email> <AdminLevel> <Hive> <Job Position>")
     public void AddReviewer(String firstName, String lastName, String email, String adminLevel, String hive, String jobPosition) {
 
-        // Click "Add Reviewer"
+        // Click Manage Reviewer --> "Add Reviewer"
 
-        Driver.webDriver.findElement(By.xpath("//*[@id=\"main-app-area\"]/div/div/div[2]/div/div[2]/div[1]/div[3]")).click();
+        Driver.webDriver.findElement(By.cssSelector("#main-app-area > div > div > div.body-admin > div > div.menu-staff > div.menu-staff-left > div:nth-child(3)")).click();
         assertThat(Driver.webDriver.findElement(By.xpath("//*[@id=\"add-a-new-reviewer\"]/div/div[1]")).getText().contains("Add a new reviewer"));
 
-        // Identity Reviewer Details Page & Enter details
+        // Assert Reviewer Details Page & Enter details
 
         Driver.webDriver.findElement(By.xpath("//*[@id=\"add-a-new-reviewer\"]/div/div[1]")).click();
         assertThat(Driver.webDriver.findElement(By.xpath("//*[@id=\"main-app-area\"]/div/div/div[1]/div/div[2]/div/div[1]/div[2]")).getText().contains("REVIEWER DETAILS"));
@@ -119,8 +131,38 @@ public class StepImplementation {
         Select JobPosition = new Select(Driver.webDriver.findElement(By.xpath("//*[@id=\"main-app-area\"]/div/div/div[1]/div/div[2]/div/div[2]/div[3]/div[3]/select")));
         JobPosition.selectByVisibleText(jobPosition);
 
+        String status = Driver.webDriver.findElement(By.className("new-reviewer-send-button")).getAttribute("disabled");
+        System.out.println(status);
+        assertThat(status).isNull();
+
+
+        Driver.webDriver.findElement(By.xpath("//*[@id=\"main-app-area\"]/div/div/div[1]/div/div[2]/div/div[2]/div[4]/div")).click();
+
+
+        try {
+            TimeUnit.SECONDS.sleep(15);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //Verify Reviewer in "Manage Reviewers"
+
+        String tableValue = Driver.webDriver.findElement(By.xpath("//*[@id=\"add-a-new-reviewer\"]/div/div[5]/div/div")).getText();
+
+        List<String> tableListValues = Arrays.asList(tableValue.split("\n"));
+        String checkPoint = firstName + " " + lastName;
+
+        int occurrences = Collections.frequency(tableListValues,checkPoint);
+
+        assertThat(occurrences).isEqualTo(1);
+
+        int i = tableListValues.indexOf(checkPoint);
+
+        assertThat(i).isGreaterThanOrEqualTo(0);
+        assertThat(checkPoint).isEqualToIgnoringCase(tableListValues.get(i).trim());
+        assertThat(jobPosition).isEqualToIgnoringCase(tableListValues.get(++i).trim());
+        assertThat(hive).isEqualToIgnoringCase(tableListValues.get(++i).trim());
+        assertThat(adminLevel).isEqualToIgnoringCase(tableListValues.get(++i).trim());
 
     }
-
-
 }
